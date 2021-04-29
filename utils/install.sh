e8ba51fc4a6201a5f4c0f091c09afc72a9bf9d98
Eric Pellegrini <ericpellegrini@outlook.com>
2021-04-29 16:48:22 +0200

    New installation script to use submodules.

--- utils/install.sh
+++ utils/install.sh
@@ -1,21 +1,18 @@
-conda activate lvmdrp
+#!/usr/bin/bash
+
+#conda activate lvmdrp
 pip3 install numpy scipy astropy pyyaml requests fitsio matplotlib
 pip3 install h5py speclite
 
-pip3 install -e desiutil --user
-pip3 install -e specter --user
-pip3 install -e desicmx --user
-pip3 install -e desidatamodel --user
-pip3 install -e desietc --user
-pip3 install -e desimodel --user
-pip3 install -e desisim --user
-pip3 install -e desispec --user
-pip3 install -e desisurvey --user
-pip3 install -e desitarget --user
-pip3 install -e desitemplate --user
-pip3 install -e fiberassign --user
-pip3 install -e prospect --user
-pip3 install -e redrock --user
-pip3 install -e surveysim --user
+
+pref="${LVMDRP}/desihub/"
+
+cd ${pref}
+
+for pkg in desiutil desimodel desitarget desisim specter desispec specex; do
+    pip3 uninstall ${pkg}
+    pip3 install -e ${pkg} --user
+done
 
 mkdir $DESIOUT
+
