08e4bad4cce2e97bd110e1e8b08d3892d161ef74
Alfredo Mejia-Narvaez <alfredoj.32@gmail.com>
2022-11-24 09:56:10 -0600

    updating sky module after Heildelberg meeting

--- python/lvmdrp/core/sky.py
+++ python/lvmdrp/core/sky.py
@@ -1,6 +1,7 @@
 import os
 import json, yaml
 import numpy as np
+from scipy.stats import zscore
 from io import BytesIO
 from astropy.io import fits
 from astropy.table import Table
@@ -57,11 +58,19 @@ PAR_MAP_R = {'INPUT_OBJECT_SPECTRUM': 'objfile',
              'FITLIM': 'fitLim',
              'PLOT_TYPE': 'plotType'}
 
-def _load_fiber_map():
+
+def _load_sky_lines():
     pass
 
 
-def _load_sky_lines():
+def _get_bright_fiber_selection(rss):
+    """Returns a selection (mask) of fibers with known stars given an RSS object
+    
+    TODO: ask Kathryn about this, she and Max have made progress on identifying bright fibers
+    """
+    # extract tile information from rss header
+    # look in Gaia tables (DB) for stars in that tile
+    # identify fibers with stellar sources and build a mask (1: bright, 0: dark)
     pass
 
 
@@ -155,88 +164,87 @@ def run_skycorr(config_file, wl, sci_rss, sky_rss, metadata=None):
     return skycorr_config_, par_file
 
 
-def select_sky_fibers(rss, fiber_map_path):
-    """select sky fibers to be used to build master skies 1 and 2"""
-    fiber_map = _load_fiber_map(fiber_map_path)
-    sky1_fibers, sky2_fibers, sci_fibers, std_fibers = rss[fiber_map["SKY_1"]], rss[fiber_map["SKY_2"]], rss[fiber_map["SCI"]], rss[fiber_map["STD"]]
-    return sky1_fibers, sky2_fibers, sci_fibers, std_fibers
+def select_sky_fibers(sky1_rss, sky2_rss, sci_rss, masked_value=np.nan, nsigma=3, outlier_thresh=0.8):
+    """select sky fibers to be used to build master skies 1 and 2
+    
+    take a look at constructSkySpec_drp which already selects fibers and computes the averaged sky
+    """
+    # clean at pixel level
+    sky1_fibers = sky1_rss._data
+    sky1_fibers[sky1_rss._mask] = masked_value
+
+    sky2_fibers = sky2_rss._data
+    sky2_fibers[sky2_rss._mask] = masked_value
+
+    sci_fibers = sci_rss._data
+    sci_fibers[sci_rss._mask] = masked_value
+
+    # TODO: interpolate masked pixels?
+    
+    # remove outlying fibers
+    sigma1 = zscore(sky1_fibers, axis=0)
+    sel_fib1 = (nsigma < sigma1).sum(axis=1) < outlier_thresh * sigma1.shape[0]
+    sky1_fibers = sky1_fibers[sel_fib1]
+    
+    sigma2 = zscore(sky2_fibers, axis=0)
+    sel_fib2 = (nsigma < sigma2).sum(axis=1) < outlier_thresh * sigma2.shape[0]
+    sky2_fibers = sky2_fibers[sel_fib2]
+
+    # outliers in science
+    # TODO: bright fibers selection with Gaia
+    bright_fibers = _get_bright_fiber_selection(sci_rss)
+    sci_fibers = sci_fibers[~bright_fibers]
+    sigma = zscore(sci_fibers, axis=0)
+    sel_fib = (nsigma < sigma).sum(axis=1) < outlier_thresh * sigma.shape[0]
+    sci_fibers = sci_fibers[sel_fib]
+
+    return sky1_fibers, sky2_fibers, sci_fibers
 
 
-def build_master_sky(sky1_fibers, sky2_fibers, metadata, method="naive"):
+def build_master_sky(sky1_fibers, sky2_fibers, sci_fibers):
+    """build master sky spectra 1 and 2 (sky1, sky2) and master science spectrum (sci1, rejecting bright fibers)
+    """
+    # average the cleaned fibers
+    master_sky1 = np.average(sky1_fibers)
+    master_sky2 = np.average(sky2_fibers)
+    master_sci = np.average(sci_fibers)
+    return master_sky1, master_sky2, master_sci
+
+def cont_line_separation(wl_master_sky, master_sky1, master_sky2):
     """run continuum/line separation algorithm on master sky 1 and 2 and master science and produce line-only (sky1_line, sky2_line, sci_line) and continuum-only (sky1_cont, sky2_cont, sci_cont) spectra for each
-    
-    Parameters
-    ----------
-    metadata: list_like
-        observing conditions for each pointing needed to evaluate ESO skycalc
     """
-    if method == "naive":
-        # deal with bad pixels (pixmask, pixel outliers)
-        # deal with outlying fibers (remove fibers with stellar contribution)
-        # match wavelength sampling between fibers
-        # average the cleaned fibers
-        pass
-    elif method == "smart":
-        # ESO way
-        pass
-    else:
-        raise ValueError
-    
-
-def cont_line_separation(wl_master_sky, master_sky1, master_sky2, sky_lines_list, window_width):
-    """average sky1_line and sky2_line into "sky_line", and run skycorr on "sky_line" and "sci_line" to produce "sky_line_corr"
-    """
-    # TODO: ask Brian about the sky lines list, I think they used something similar for MaNGA
-    wl_sky_lines, sky_lines = _load_sky_lines(sky_lines_list)
-
-    # the window size may be a function of the wavelength (there may be sky line groupings)
-    # hw = window_width / 2
-    # lines_mask = np.ones_like(wl_sky, dtype=bool)
-    # for wl_sky in wl_sky_lines:
-    #     lines_mask &= (wl_sky-hw<=wl_master_sky)&(wl_master_sky<=wl_sky+hw)
-    # return master_sky2_cont, master_sky2_cont, master_sky1_line, master_sky2_line
+    # build a sky model library with continuum and line separated (ESO skycalc)
+    # use this library as templates to fit master skies
+    # check if we can recover observing condition parameters from this fit
     pass
 
 
-def eval_eso_sky(sky1_fibers, sky2_fibers, sci_fibers, metadata):
+def sky_line_correct(sky1_line, sky2_line):
+    """average sky1_line and sky2_line into 'sky_line', and run skycorr on 'sky_line' and 'sci_line' to produce 'sky_line_corr'"""
+    # compute a weighted average using as weights the inverse distance distance to science
+    # run skycorr on averaged line spectrum
+    pass
+
+
+def eval_eso_sky(sky1_par, sky2_par, sci_par):
     """run ESO sky model for observation parameters (ephemeris, atmospheric conditions, site, etc) to evaluate sky spectrum at each telescope pointing (model_sky1, model_sky2, model_skysci)
     
-
-
     Returns
     -------
     sky_line_corr, sky_cont_corr, model_skysci
 
     """
-    sky_metas, sky_comps = [], []
-    for meta in metadata:
-        sky_meta, sky_comp = get_sky_model(**meta.__dict__)
-        sky_metas.append(sky_meta)
-        sky_comps.append(sky_comp)
-    
-    # one scaling for lines and one for the cont
-    sky1_scale = None
-    sky1_line_corr = sky1_scale * sky_comps[0]["flux"]/sky_comps[0]["trans"]
-    sky1_cont_corr = sky1_scale * sky_comps[0]["trans"]
+    sky1_meta, sky1_model = get_sky_model(**sky1_par)
+    sky2_meta, sky2_model = get_sky_model(**sky2_par)
+    sci_meta, sci_model = get_sky_model(**sci_par)    
 
-    sky2_scale = None
-    sky2_line_corr = sky2_scale * sky_comps[1]["flux"]/sky_comps[1]["trans"]
-    sky2_cont_corr = sky2_scale * sky_comps[1]["trans"]
-
-    # eval ESO sky in sci pointings
-    model_skysci = None
-
-    # compute sky_line_corr and sky_cont_corr
-    sky_line_corr = None
-    sky_cont_corr = None
-
-    return sky_line_corr, sky_cont_corr, model_skysci
+    return sky1_model, sky2_model, sci_model
 
 
-def sky_cont_correct(master_sky1_cont, master_sky2_cont, model_skysci):
+def sky_cont_correct(sky1_cont, sky2_cont, sky1_model, sky2_model, sci_model):
     """correct and combine continuum only spectra by doing:   sky_cont_corr=0.5*( sky1_cont*(model_skysci/model_sky1) + sky2_cont*(model_skysci/model_sky2)) """
-    # return sky_cont_corr
-    pass
+    sky_cont_corr = 0.5 * (sky1_cont * (sci_model/sky1_model) + sky2_cont * (sci_model/sky2_model))
+    return sky_cont_corr
 
 
 def coadd_sky(sky_cont_corr, sky_line_corr):
