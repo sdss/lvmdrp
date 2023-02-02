3197c8147f51e1eb18d374365430cd07316d7019
Alfredo Mejia-Narvaez <alfredoj.32@gmail.com>
2023-02-02 09:43:37 -0600

    adding astrometry module start

---
+++ python/lvmdrp/functions/astrometryMethod.py
@@ -0,0 +1,19 @@
+# encoding: utf-8
+#
+# @Author: Alfredo Mejía-Narváez
+# @Date: Feb 1, 2023
+# @Filename: astrometryMethod.py
+# @License: BSD 3-Clause
+# @Copyright: SDSS-V LVM
+
+# TODO: implement downloading reference images from PanSTARRS for all LVM pointings
+def createReferenceLibrary_drp():
+    pass
+
+# TODO: implement astrometry solution for a given RSS
+def astrometricSolution_drp():
+    pass
+
+# TODO: fine astrometric solution
+def refineAstrometry_drp():
+    pass
