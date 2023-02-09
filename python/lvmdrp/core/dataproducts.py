a68357aa7f557a2c1c3151d04954c1fc3cc8840f
Alfredo Mejia-Narvaez <alfredoj.32@gmail.com>
2023-02-09 09:57:45 -0600

    updating dataproduct BPs and module

--- python/lvmdrp/core/dataproducts.py
+++ python/lvmdrp/core/dataproducts.py
@@ -10,6 +10,8 @@ import os
 import yaml
 import numpy as np
 from astropy.io import fits
+from astropy.table import Table
+from astropy import units as u
 
 from lvmdrp.core.constants import CONFIG_PATH
 
@@ -33,10 +35,16 @@ def load_blueprint(name):
             a dictionary containing a dataproduct definition
 
     """
-    pass
+    if not name.endswith(".yaml"):
+        _name = f"{name}.yaml"
+    else:
+        _name = name
+    
+    dataproduct_bp = yaml.safe_load(os.path.join(DATAPRODUCT_BLUEPRINTS_PATH, _name))
+    return dataproduct_bp
 
 
-def dump_template(dataproduct_bp, name, path):
+def dump_template(dataproduct_bp, save=False):
     """
         Returns a FITS object following the given blueprint
 
@@ -44,6 +52,9 @@ def dump_template(dataproduct_bp, name, path):
         ----------
         dataproduct_bp: dict_like
             a dictionary containing a dataproduct definition
+        save: boolean
+            whether to save the template in the original
+            blueprint directory with name {name}_template.fits.gz
 
         Returns
         -------
@@ -51,7 +62,33 @@ def dump_template(dataproduct_bp, name, path):
             a FITS template for a particular dataproduct
 
     """
-    pass
+    hdu_list = []
+    for ihdu in filter(lambda key: key.startswith("hdu"), dataproduct_bp):
+        hdu_bp = dataproduct_bp[ihdu]
+
+        if hdu_bp["is_image"]:
+            header = fits.Header([(card.get("key"), card.get("value"), card.get("comment")) for card in hdu_bp.get("header", [])])
+            header["COMMENT"] = hdu_bp["description"]
+            if ihdu == "hdu0":
+                hdu = fits.PrimaryHDU(header=header)
+            else:
+                header.name = hdu_bp["name"]
+                hdu = fits.ImageHDU(header=header)
+        else:
+            header = fits.Header()
+            header["COMMENT"] = hdu_bp["description"]
+            header.name = hdu_bp["name"]
+            cols = [fits.Column(name=col["name"], format=col["type"], unit=col["unit"]) for _, col in hdu_bp.get("columns", {}).items()]
+            hdu = fits.BinTableHDU.from_columns(cols, header=header)
+        
+        hdu_list.append(hdu)
+
+    fits_template = fits.HDUList(hdus=hdu_list)
+    if save:
+        _name = f"{dataproduct_bp['name']}_template.fits.gz"
+        fits_template.writeto(os.path.join(DATAPRODUCT_BLUEPRINTS_PATH, _name))
+    
+    return fits_template
 
 
 def fill_template(fits_template, **kwargs):
