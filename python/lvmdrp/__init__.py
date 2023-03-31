6bc1cd062a1d7bcfda0ad26a6b126a8e5a22420a
Brian Cherinka <havok2063@hotmail.com>
2023-03-31 15:34:50 -0400

    adding sdsstools req for version and logger

--- python/lvmdrp/__init__.py
+++ python/lvmdrp/__init__.py
@@ -1,15 +1,14 @@
-#import fit_profile
-#import pyfits, numpy, pylab, sys, time
-#from types import *
-#import exceptions, copy_reg
-#from copy import deepcopy
-#from scipy import ndimage
-#from scipy import signal
-#from scipy import optimize
-#from scipy import sparse
-#from scipy import interpolate
-#from multiprocessing import Pool
-#from multiprocessing import cpu_count
+#!/usr/bin/env python
+# encoding: utf-8
 
-from . import external, functions
-from .core import *
+from sdsstools import get_logger, get_package_version
+
+
+NAME = 'lvmdrp'
+
+
+# init the logger
+log = get_logger(NAME)
+
+
+__version__ = get_package_version(path=__file__, package_name=NAME)
