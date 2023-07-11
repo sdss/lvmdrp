0746e235f99bd43df07ae2fd631550a8b8cb81ee
Alfredo Mejia-Narvaez <alfredoj.32@gmail.com>
2023-07-10 20:14:09 -0600

    creating .reg file at fiber location task

--- python/lvmdrp/functions/imageMethod.py
+++ python/lvmdrp/functions/imageMethod.py
@@ -834,6 +834,19 @@ def find_peaks_auto(
     for i in range(len(centers)):
         file_out.write("%i %i %e %i\n" % (i, round_cent[i], centers[i], 0))
     file_out.close()
+    # write .reg file for ds9
+    file_out = open(out_peaks.replace(".txt", f"-{column}.reg"), "w")
+    file_out.write("# Region file format: DS9 version 4.1\n")
+    file_out.write(
+        'global color=green dashlist=8 3 width=1 font="helvetica 10 normal roman" select=1 highlite=1 dash=0 fixed=0 edit=1 move=1 delete=1 include=1 source=1\n'
+    )
+    file_out.write("physical\n")
+    for i in range(len(centers)):
+        file_out.write(
+            "# text(%.4f,%.4f) text={%i, %i}\n"
+            % (column, centers[i], i + 1, round_cent[i])
+        )
+    file_out.close()
 
     # plot figure
     fig, ax = create_subplots(to_display=display_plots, figsize=(15, 10))
