5f60481ca22f802b053d70fdf651f7fb5a17443f
Brian Cherinka <havok2063@hotmail.com>
2023-07-13 16:03:10 -0400

    rebumping

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ from setuptools import find_packages, setup
 # The NAME variable should be of the format "sdss-drp".
 # Please check your NAME adheres to that format.
 NAME = "lvmdrp"
-VERSION = "0.1.0"
+VERSION = "0.1.1dev"
 
 
 def run(packages, install_requires):
