532b7115982b954fd5d50236e8d4d639d26a4e84
Alfredo Mejia-Narvaez <alfredoj.32@gmail.com>
2023-07-26 21:09:24 -0600

    adding script for super traces

--- setup.py
+++ setup.py
@@ -34,7 +34,12 @@ def run(packages, install_requires):
         packages=packages,
         install_requires=install_requires,
         package_dir={"": "python"},
-        scripts=["bin/drp", "bin/pix2wave"],
+        scripts=["bin/drp", "bin/pix2wave",
+            "bin/build_super_pixmask",
+            "bin/build_super_arc",
+            "bin/build_super_waves",
+            "bin/build_super_trace"
+        ],
         classifiers=[
             "Development Status :: 4 - Beta",
             "Intended Audience :: Science/Research",
