4dc2d4a5b2e86261aa304c9a0c91d4d8e7c48ea1
Alfredo Mejia-Narvaez <alfredoj.32@gmail.com>
2023-08-16 22:19:35 -0600

    adding sky subtraction to quick DRP

--- python/lvmdrp/functions/run_quickdrp.py
+++ python/lvmdrp/functions/run_quickdrp.py
@@ -18,6 +18,7 @@ from lvmdrp.utils import metadata as md
 from lvmdrp.functions import run_drp as drp
 from lvmdrp.functions import imageMethod as image_tasks
 from lvmdrp.functions import rssMethod as rss_tasks
+from lvmdrp.functions import skyMethod as sky_tasks
 from lvmdrp.core.constants import SPEC_CHANNELS
 
 
@@ -56,6 +57,8 @@ def quick_reduction(expnum: int, use_fiducial_master: bool = False) -> None:
         xsci_path = path.full("lvm_anc", drpver=drpver, kind="x", imagetype=sci["imagetyp"], **sci)
         wsci_path = path.full("lvm_anc", drpver=drpver, kind="w", imagetype=sci["imagetyp"], **sci)
         hsci_path = path.full("lvm_anc", drpver=drpver, kind="h", imagetype=sci["imagetyp"], **sci)
+        hskye_path = path.full("lvm_anc", drpver=drpver, kind="h", imagetype="sky_e", **sci)
+        hskyw_path = path.full("lvm_anc", drpver=drpver, kind="h", imagetype="sky_w", **sci)
         os.makedirs(os.path.dirname(hsci_path), exist_ok=True)
         # define current arc lamps to use for wavelength calibration
         lamps = arc_lamps[sci_camera[0]]
@@ -95,13 +98,29 @@ def quick_reduction(expnum: int, use_fiducial_master: bool = False) -> None:
         # extract 1d spectra
         image_tasks.extract_spectra(in_image=dsci_path, out_rss=xsci_path, in_trace=mtrace_path, method="aperture", aperture=3)
         
-        # wavelength calibrate & resample
-        iwave, fwave = SPEC_CHANNELS[sci_camera[0]]
+        # wavelength calibrate
         rss_tasks.create_pixel_table(in_rss=xsci_path, out_rss=wsci_path, arc_wave=mwave_path, arc_fwhm=mlsf_path)
-        rss_tasks.resample_wavelength(in_rss=wsci_path, out_rss=hsci_path, method="linear", disp_pix=0.5, start_wave=iwave, end_wave=fwave, err_sim=10, parallel=0)
-        
+
+        # interpolate & subtract sky fibers
+        # sky_tasks.interpolate_sky(in_rss=wsci_path, out_sky=hskye_path, which="both", subtract=True, out_rss=wsci_path)
+    
+        # resample in wavelength
+        iwave, fwave = SPEC_CHANNELS[sci_camera[0]]
+        rss_tasks.resample_wavelength(in_rss=wsci_path, out_rss=hsci_path, method="linear", disp_pix=0.5, start_wave=iwave, end_wave=fwave, err_sim=10, parallel=0, extrapolate=False)
+
         # apply fiberflat correction
         rss_tasks.apply_fiberflat(in_rss=hsci_path, out_rss=hsci_path, in_flat=mflat_path)
+
+        # interpolate sky fibers
+        sky_tasks.interpolate_sky(in_rss=wsci_path, out_sky=hskye_path, which="e")
+        rss_tasks.resample_wavelength(in_rss=hskye_path, out_rss=hskye_path, method="linear", disp_pix=0.5, start_wave=iwave, end_wave=fwave, err_sim=10, parallel=0, extrapolate=False)
+        rss_tasks.apply_fiberflat(in_rss=hskye_path, out_rss=hskye_path, in_flat=mflat_path)
+        sky_tasks.interpolate_sky(in_rss=wsci_path, out_sky=hskyw_path, which="w")
+        rss_tasks.resample_wavelength(in_rss=hskyw_path, out_rss=hskyw_path, method="linear", disp_pix=0.5, start_wave=iwave, end_wave=fwave, err_sim=10, parallel=0, extrapolate=False)
+        rss_tasks.apply_fiberflat(in_rss=hskyw_path, out_rss=hskyw_path, in_flat=mflat_path)
+
+        # quick sky subtraction
+        sky_tasks.quick_sky_subtraction(in_rss=hsci_path, out_rss=hsci_path, in_skye=hskye_path, in_skyw=hskyw_path)
 
     # combine channels
     drp.combine_cameras(sci_tileid, sci_mjd, expnum=sci_expnum, spec=1)
