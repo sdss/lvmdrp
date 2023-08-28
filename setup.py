e8b3a01a3843cab3419c3c7bf548241cce9045f0
Alfredo Mejia-Narvaez <alfredoj.32@gmail.com>
2023-08-27 19:51:18 -0600

    superseding requirements.txt, setup.py update & adding enviroment check tool

--- setup.py
+++ setup.py
@@ -2,43 +2,57 @@
 #
 # setup.py
 
-
-from __future__ import absolute_import, division, print_function
-
-import os
-import sys
-
-from setuptools import find_packages, setup
+from setuptools import setup
 
 
-# The NAME variable should be of the format "sdss-drp".
-# Please check your NAME adheres to that format.
 NAME = "lvmdrp"
 VERSION = "0.1.1dev"
 
 
-def run(packages, install_requires):
+def run():
     setup(
         name=NAME,
         version=VERSION,
         license="BSD3",
         description="SDSSV-LVM Data Reduction Pipeline",
         long_description=open("README.rst").read(),
-        author="Eric Pellegrini",
-        author_email="ericpellegrini@outlook.com",
+        author="Alfredo Mejia-Narvaez, Eric Pellegrini",
+        author_email="alfredoj.32@gmail.com, ericpellegrini@outlook.com",
         keywords="astronomy software",
         url="https://github.com/sdss/lvmdrp",
         include_package_data=True,
         python_requires=">=3.8",
-        packages=packages,
-        install_requires=install_requires,
-        package_dir={"": "python"},
-        scripts=["bin/drp", "bin/pix2wave",
-            "bin/build_super_pixmask",
-            "bin/build_super_arc",
-            "bin/build_super_waves",
-            "bin/build_super_trace"
+        install_requires=[
+            "numpy>=1.24.1",
+            "scipy>=1.10.1",
+            "matplotlib>=3.6.2",
+            "pandas>=1.5.3",
+            "tqdm>=4.64.1",
+            "astropy>=5.2",
+            "ccdproc>=2.4.0",
+            "dust_extinction>=1.1",
+            "skyfield>=1.45",
+            "pydl>=0.7.0",
+            "skycalc_cli==1.4",
+            "sdsstools>=1.0",
+            "sdss-tree>=4.0",
+            "sdss-access>=3.0",
+            "click>=8.0",
+            "cloup>=2.0",
+            "bottleneck>=1.3.7",
+            "h5py>=3.8.0"
         ],
+        extras_require={
+            "dev": [
+                "ipython>=8.10.0",
+                "jupyter>=1.0.0",
+                "jupyterlab>=3.6.1",
+                "ipywidgets>=8.1.0"
+            ]
+        },
+        packages=["lvmdrp"],
+        package_dir={"": "python"},
+        scripts=["bin/drp", "bin/pix2wave", "bin/envcheck"],
         classifiers=[
             "Development Status :: 4 - Beta",
             "Intended Audience :: Science/Research",
@@ -52,51 +66,5 @@ def run(packages, install_requires):
         ],
     )
 
-
-def parse_requirements(reqfile_path):
-    """
-    Returns the parsed requirements from a requirements .txt file
-    """
-    install_requires = []
-    with open(reqfile_path, "r") as r:
-        for requirement in r.readlines():
-            requirement = requirement.strip()
-            if requirement.startswith("-r"):
-                install_requires.extend(
-                    parse_requirements(requirement.replace("-r ", ""))
-                )
-            else:
-                install_requires.append(requirement)
-    return install_requires
-
-
-def get_requirements():
-    """Get the proper requirements file based on the optional argument"""
-
-    requirements_file = os.path.join(os.path.dirname(__file__), "requirements.txt")
-    install_requires = parse_requirements(requirements_file)
-    return install_requires
-
-
-def remove_args(parser):
-    """Remove custom arguments from the parser"""
-
-    arguments = []
-    for action in list(parser._get_optional_actions()):
-        if "--help" not in action.option_strings:
-            arguments += action.option_strings
-
-    for arg in arguments:
-        if arg in sys.argv:
-            sys.argv.remove(arg)
-
-
 if __name__ == "__main__":
-    # Get the proper requirements file
-    install_requires = get_requirements()
-
-    # Have distutils find the packages
-    packages = find_packages(where="python")
-
-    # Runs distutils
-    run(packages, install_requires)
+    run()
