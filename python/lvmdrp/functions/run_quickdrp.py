e031a8522cf8163fdc3728f97410688a521770fa
Alfredo Mejia-Narvaez <alfredoj.32@gmail.com>
2023-09-19 11:02:37 -0600

    fixing flatfields across telescopes

--- python/lvmdrp/functions/run_quickdrp.py
+++ python/lvmdrp/functions/run_quickdrp.py
@@ -10,6 +10,7 @@
 import os
 import click
 import cloup
+import numpy as np
 
 from astropy.table import Table
 
@@ -24,6 +25,51 @@ from lvmdrp.core.constants import SPEC_CHANNELS
 
 ORIG_MASTER_DIR = os.getenv("LVM_MASTER_DIR")
 
+
+def illumination_correction(in_fiberflat=None):
+
+    if in_fiberflat is None:
+        factors = {('SkyW', 'b1'): 0.8586127,
+        ('SkyE', 'b1'): 1.0431333,
+        ('SkyW', 'r1'): 0.9103954,
+        ('SkyE', 'r1'): 0.980759,
+        ('SkyW', 'z1'): 0.8883656,
+        ('SkyE', 'z1'): 0.956361,
+        ('SkyW', 'b2'): 0.89451915,
+        ('SkyE', 'b2'): 1.03991,
+        ('SkyW', 'r2'): 0.91166383,
+        ('SkyE', 'r2'): 0.975086,
+        ('SkyW', 'z2'): 0.89231634,
+        ('SkyE', 'z2'): 0.95734394,
+        ('SkyW', 'b3'): 0.83859897,
+        ('SkyE', 'b3'): 1.0092189,
+        ('SkyW', 'r3'): 0.8806886,
+        ('SkyE', 'r3'): 0.962306,
+        ('SkyW', 'z3'): 0.8598578,
+        ('SkyE', 'z3'): 0.95633197}
+
+        return factors
+    
+    fibers = [2, 106, 37]
+    fibermap = Table(drp.fibermap.data)
+    select = np.isin(fibermap["fiberid"]-1, fibers)
+    skw1_fibers = fibermap[~select][fibermap[~select]["ifulabel"] == "SkyW1"]["fiberid"]-1
+    ske1_fibers = fibermap[~select][fibermap[~select]["ifulabel"] == "SkyE1"]["fiberid"]-1
+    sci1_fibers = fibermap[~select][fibermap[~select]["ifulabel"] == "Sci1"]["fiberid"]-1
+
+    factors = {}
+    for spec in "123":
+        for cam in ["b", "r", "z"]:
+            cam = cam + spec
+            fflat = rss_tasks.RSS()
+            fflat.loadFitsData(f"/home/mejia/Research/lvm/lvmdata/calib/60177/lvm-mfiberflat-{cam}.fits")
+            fflat._data[(fflat._mask)|(fflat._data <= 0)] = np.nan
+            sci_factor = np.nanmedian(fflat._data[sci1_fibers][:, 1000:3000])
+            print(cam, np.nanmedian(fflat._data[skw1_fibers][:, 1000:3000])/sci_factor, np.nanmedian(fflat._data[ske1_fibers][:, 1000:3000])/sci_factor)
+            factors[("SkyW", cam)] = np.nanmedian(fflat._data[skw1_fibers][:, 1000:3000])/sci_factor
+            factors[("SkyE", cam)] = np.nanmedian(fflat._data[ske1_fibers][:, 1000:3000])/sci_factor
+    
+    return factors
 
 def get_master_mjd(sci_mjd):
 
@@ -67,15 +113,19 @@ def quick_reduction(expnum: int, use_fiducial_master: bool = False) -> None:
         # define science camera
         sci_camera = sci["camera"]
 
+        # if sci_camera != "r1": continue
+
         # define sci paths
         sci_path = path.full("lvm_raw", camspec=sci_camera, **sci)
         psci_path = path.full("lvm_anc", drpver=drpver, kind="p", imagetype=sci["imagetyp"], **sci)
         dsci_path = path.full("lvm_anc", drpver=drpver, kind="d", imagetype=sci["imagetyp"], **sci)
         xsci_path = path.full("lvm_anc", drpver=drpver, kind="x", imagetype=sci["imagetyp"], **sci)
         wsci_path = path.full("lvm_anc", drpver=drpver, kind="w", imagetype=sci["imagetyp"], **sci)
+        fsci_path = path.full("lvm_anc", drpver=drpver, kind="f", imagetype=sci["imagetyp"], **sci)
+        ssci_path = path.full("lvm_anc", drpver=drpver, kind="s", imagetype=sci["imagetyp"], **sci)
         hsci_path = path.full("lvm_anc", drpver=drpver, kind="h", imagetype=sci["imagetyp"], **sci)
-        wskye_path = path.full("lvm_anc", drpver=drpver, kind="w", imagetype="sky_e", **sci)
-        wskyw_path = path.full("lvm_anc", drpver=drpver, kind="w", imagetype="sky_w", **sci)
+        fskye_path = path.full("lvm_anc", drpver=drpver, kind="f", imagetype="sky_e", **sci)
+        fskyw_path = path.full("lvm_anc", drpver=drpver, kind="f", imagetype="sky_w", **sci)
         hskye_path = path.full("lvm_anc", drpver=drpver, kind="h", imagetype="sky_e", **sci)
         hskyw_path = path.full("lvm_anc", drpver=drpver, kind="h", imagetype="sky_w", **sci)
         os.makedirs(os.path.dirname(hsci_path), exist_ok=True)
@@ -93,6 +143,7 @@ def quick_reduction(expnum: int, use_fiducial_master: bool = False) -> None:
             mdark_path = os.path.join(masters_path, f"lvm-mdark-{sci_camera}.fits")
             mpixflat_path = os.path.join(masters_path, f"lvm-mpixflat-{sci_camera}.fits")
             mtrace_path = os.path.join(masters_path, f"lvm-mtrace-{sci_camera}.fits")
+            mwidth_path = os.path.join(masters_path, f"lvm-mwidth-{sci_camera}.fits")
             macorr_path = os.path.join(masters_path, f"lvm-apercorr-{sci_camera}.fits")
             mwave_path = os.path.join(masters_path, f"lvm-mwave_{lamps}-{sci_camera}.fits")
             mlsf_path = os.path.join(masters_path, f"lvm-mlsf_{lamps}-{sci_camera}.fits")
@@ -107,7 +158,8 @@ def quick_reduction(expnum: int, use_fiducial_master: bool = False) -> None:
             mdark_path = path.full("lvm_master", drpver=drpver, kind="mdark", **masters["dark"].to_dict())
             mpixflat_path = None
             mtrace_path = path.full("lvm_master", drpver=drpver, kind="mtrace", **masters["trace"].to_dict())
-            macorr_path = path.full("lvm_master", drpver=drpver, kind="apercorr", **masters["acorr"].to_dict())
+            mwidth_path = None
+            macorr_path = None
             mwave_path = path.full("lvm_master", drpver=drpver, kind=f"mwave_{lamps}", **masters["wave"].to_dict())
             mlsf_path = path.full("lvm_master", drpver=drpver, kind=f"mlsf_{lamps}", **masters["lsf"].to_dict())
             mflat_path = path.full("lvm_master", drpver=drpver, kind="mfiberflat", **masters["fiberflat"].to_dict())
@@ -120,27 +172,41 @@ def quick_reduction(expnum: int, use_fiducial_master: bool = False) -> None:
                                   in_bias=mbias_path, in_dark=mdark_path, in_pixelflat=mpixflat_path,
                                   in_slitmap=Table(drp.fibermap.data), reject_cr=False)
         
-        # extract 1d spectra
-        image_tasks.extract_spectra(in_image=dsci_path, out_rss=xsci_path, in_trace=mtrace_path, in_acorr=macorr_path, method="aperture", aperture=3)
+        # # extract 1d spectra
+        # image_tasks.extract_spectra(in_image=dsci_path, out_rss=xsci_path, in_trace=mtrace_path, fwhm=mwidth_path, method="optimal", parallel=4)
+        image_tasks.extract_spectra(in_image=dsci_path, out_rss=xsci_path, in_acorr=macorr_path, in_trace=mtrace_path, method="aperture", aperture=3)
 
         # wavelength calibrate
         rss_tasks.create_pixel_table(in_rss=xsci_path, out_rss=wsci_path, arc_wave=mwave_path, arc_fwhm=mlsf_path)
 
         # apply fiberflat correction
-        rss_tasks.apply_fiberflat(in_rss=wsci_path, out_rss=wsci_path, in_flat=mflat_path)
+        rss_tasks.apply_fiberflat(in_rss=wsci_path, out_rss=fsci_path, in_flat=mflat_path)
+
+        # NOTE: this is a temporary fix for the illumination bias across telescopes whe using dome flats
+        rss = rss_tasks.RSS()
+        factors = illumination_correction(in_fiberflat=None)
+        rss.loadFitsData(fsci_path)
+        fibermap = rss._slitmap
+        fibermap = fibermap[fibermap["spectrographid"] == int(sci_camera[1])]
+        for ifiber in range(rss._fibers):
+            f = factors.get((fibermap[ifiber]["telescope"], sci_camera), 1)
+            # print(f"applying factor {(fibermap[ifiber]['telescope'], sci_camera)} : {f}")
+            rss._data[ifiber] *= f
+            rss._error[ifiber] *= f
+        rss.writeFitsData(fsci_path)
 
         # interpolate sky fibers
-        sky_tasks.interpolate_sky(in_rss=wsci_path, out_sky=wskye_path, which="e")
-        sky_tasks.interpolate_sky(in_rss=wsci_path, out_sky=wskyw_path, which="w")
+        sky_tasks.interpolate_sky(in_rss=fsci_path, out_sky=fskye_path, which="e")
+        sky_tasks.interpolate_sky(in_rss=fsci_path, out_sky=fskyw_path, which="w")
 
         # quick sky subtraction
-        sky_tasks.quick_sky_subtraction(in_rss=wsci_path, out_rss=wsci_path, in_skye=wskye_path, in_skyw=wskyw_path)
+        sky_tasks.quick_sky_subtraction(in_rss=fsci_path, out_rss=ssci_path, in_skye=fskye_path, in_skyw=fskyw_path)
 
         # resample wavelength into uniform grid along fiber IDs
         iwave, fwave = SPEC_CHANNELS[sci_camera[0]]
-        rss_tasks.resample_wavelength(in_rss=wsci_path, out_rss=hsci_path, method="linear", disp_pix=0.5, start_wave=iwave, end_wave=fwave, err_sim=10, parallel=0, extrapolate=False)
-        rss_tasks.resample_wavelength(in_rss=wskye_path, out_rss=hskye_path, method="linear", disp_pix=0.5, start_wave=iwave, end_wave=fwave, err_sim=10, parallel=0, extrapolate=False)
-        rss_tasks.resample_wavelength(in_rss=wskyw_path, out_rss=hskyw_path, method="linear", disp_pix=0.5, start_wave=iwave, end_wave=fwave, err_sim=10, parallel=0, extrapolate=False)
+        rss_tasks.resample_wavelength(in_rss=ssci_path, out_rss=hsci_path, method="linear", disp_pix=0.5, start_wave=iwave, end_wave=fwave, err_sim=10, parallel=0, extrapolate=False)
+        rss_tasks.resample_wavelength(in_rss=fskye_path, out_rss=hskye_path, method="linear", disp_pix=0.5, start_wave=iwave, end_wave=fwave, err_sim=10, parallel=0, extrapolate=False)
+        rss_tasks.resample_wavelength(in_rss=fskyw_path, out_rss=hskyw_path, method="linear", disp_pix=0.5, start_wave=iwave, end_wave=fwave, err_sim=10, parallel=0, extrapolate=False)
 
     # combine channels
     drp.combine_cameras(sci_tileid, sci_mjd, expnum=sci_expnum, spec=1)
