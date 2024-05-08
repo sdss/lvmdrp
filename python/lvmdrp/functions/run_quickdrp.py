7dc464348e4236ed94b8b4dd0300d3159129c495
Alfredo Mejia-Narvaez <alfredoj.32@gmail.com>
2024-05-08 19:35:21 -0400

    implementing stray light subtraction for science reductions

--- python/lvmdrp/functions/run_quickdrp.py
+++ python/lvmdrp/functions/run_quickdrp.py
@@ -190,12 +190,9 @@ def quick_science_reduction(expnum: int, use_fiducial_master: bool = False,
         image_tasks.add_astrometry(in_image=dsci_path, out_image=dsci_path, in_agcsci_image=agcsci_path, in_agcskye_image=agcskye_path, in_agcskyw_image=agcskyw_path)
 
         # subtract straylight
-        if sci_imagetyp == "flat":
-            image_tasks.subtract_straylight(in_image=dsci_path, out_image=lsci_path,
-                                                in_cent_trace=mtrace_path, select_nrows=5,
-                                                aperture=13, smoothing=400, median_box=21, gaussian_sigma=0.0)
-        else:
-            lsci_path = dsci_path
+        image_tasks.subtract_straylight(in_image=lsci_path, out_image=lsci_path, out_stray=lstr_path,
+                                        in_cent_trace=mtrace_path, select_nrows=(5,5), use_weights=True,
+                                        aperture=15, smoothing=400, median_box=101, gaussian_sigma=20.0)
 
         # extract 1d spectra
         image_tasks.extract_spectra(in_image=dsci_path, out_rss=xsci_path, in_trace=mtrace_path, in_fwhm=mwidth_path,
