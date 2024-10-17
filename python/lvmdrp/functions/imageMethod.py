bd0b57dd1579d5187ae4120f371b35b2dc563ee0
Alfredo Mejia-Narvaez <alfredoj.32@gmail.com>
2024-10-17 14:42:29 -0600

    fixing missing 'none' option in fix-pixel-shifts

--- python/lvmdrp/functions/imageMethod.py
+++ python/lvmdrp/functions/imageMethod.py
@@ -758,7 +758,7 @@ def fix_pixel_shifts(in_images, out_images, ref_images, in_mask, report=None,
                 [img.add_header_comment("QC reports and DRP do not agree on the shifted rows") for img in images]
                 if interactive:
                     log.info("interactive mode enabled")
-                    answer = input("apply [q]c, [d]rp or [c]ustom shifts: ")
+                    answer = input("apply [d]rp, [c]ustom shifts or [n]one: ")
                     if answer.lower() == "q":
                         log.info("choosing QC shifts")
                         shifts = qshifts
@@ -777,6 +777,14 @@ def fix_pixel_shifts(in_images, out_images, ref_images, in_mask, report=None,
                         shifts = cshifts
                         corrs = numpy.zeros_like(cshifts)
                         which_shifts = "custom"
+                    elif answer.lower() == "n":
+                        log.info("choosing to apply no shift")
+                        cshifts = numpy.zeros_like(cdata.shape[0])
+                        shifts = cshifts
+                        corrs = numpy.zeros_like(cshifts)
+                        which_shifts = "custom"
+                        apply_shifts = False
+
                     apply_shifts = numpy.any(numpy.abs(shifts)>0)
                 else:
                     log.warning(f"no shift will be applied to the images: {in_images}")
