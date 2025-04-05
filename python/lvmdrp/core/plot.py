bug fixes in gradient fitting & places

python/lvmdrp/core/plot.py
@@ -740,7 +740,8 @@ def ifu_view(slitmap=None, z=None, rss=None, cwave=None, dwave=None, comb_stat=N
         pa = 0
 
     sel_telescope = slitmap["telescope"].data == telescope
-    x, y = slitmap[xcol].data[sel_telescope], slitmap[ycol].data[sel_telescope]
+    x, y = slitmap[xcol].data, slitmap[ycol].data
+    x_, y_ = x[sel_telescope], y[sel_telescope]
 
     if rss is None and slitmap is not None and z is not None:
         z_ = z[sel_telescope]
@@ -763,7 +764,7 @@ def ifu_view(slitmap=None, z=None, rss=None, cwave=None, dwave=None, comb_stat=N
     if ax is None:
         fig, ax = plt.subplots(figsize=(5, 5), layout="constrained")
     norm = simple_norm(z_[~np.isnan(z_)], **nminmax)
-    sc = ax.scatter(x, y, c=z_, s=marker_size, marker=(6, 0, 90-pa), norm=norm, cmap=cmap)
+    sc = ax.scatter(x_, y_, c=z_, s=marker_size, marker=(6, 0, 90-pa), norm=norm, cmap=cmap)
     set_colorbar(ax, sc)
 
     if hide_axis:

python/lvmdrp/core/rss.py
@@ -5,7 +5,6 @@ import bottleneck as bn
 from copy import deepcopy as copy
 from tqdm import tqdm
 from scipy import interpolate, ndimage
-from scipy.optimize import least_squares
 from astropy.io import fits as pyfits
 from astropy.wcs import WCS
 from astropy.table import Table
@@ -25,7 +24,7 @@ from lvmdrp.core.header import Header
 from lvmdrp.core.positionTable import PositionTable
 from lvmdrp.core.spectrum1d import Spectrum1D, find_continuum
 from lvmdrp.core import dataproducts as dp
-from lvmdrp.core.fit_profile import ifu_factors, ifu_gradient, ifu_joint_model, gradient_residual
+from lvmdrp.core.fit_profile import IFUGradient
 from lvmdrp.core.resample import resample_flux_density
 from lvmdrp.core.plot import plot_gradient_fit
 
@@ -3277,28 +3276,22 @@ class RSS(FiberRows):
             z, x, y = self.fit_lines_slit(cwaves=cwave, return_xy=True, select_fibers="Sci")
         mu = numpy.nanmean(z)
         z_ = z / mu
-        x_, y_ = copy(x), copy(y)
 
-        # mask invalid spaxels
-        mask = numpy.isfinite(z_)
-        x_, y_, z_ = x[mask], y[mask], z_[mask]
-        fiber_groups_ = fiber_groups[mask]
-
-        # define guess and boundary values
-        guess_coeffs = [1, 2, 3, 4]
+        # # define guess and boundary values
+        guess_coeffs = [1, 2, 3, 0]
         guess_factors = len(set(fiber_groups)) * [1]
-        guess = guess_coeffs + guess_factors
-        bound_lower = len(guess_coeffs) * [-numpy.inf] + len(guess_factors) * [0.1]
-        bound_upper = len(guess_coeffs) * [+numpy.inf] + len(guess_factors) * [1.0]
-        fit = least_squares(gradient_residual, x0=guess, args=(x_, y_, z_, fiber_groups_), bounds=(bound_lower, bound_upper))
+        model = IFUGradient(guess_coeffs, guess_factors, fixed_coeffs=[3])
 
-        coeffs = fit.x[:len(guess_coeffs)]
-        factors = fit.x[len(guess_coeffs):]
+        mask = numpy.isfinite(z_)
+        model.fit(x[mask], y[mask], z_[mask], fiber_groups[mask])
+
+        coeffs = model._coeffs
+        factors = model._factors
         factors /= bn.nanmean(factors)
 
         if axs is not None:
-            gradient_model = ifu_gradient(coeffs, x=x, y=y, normalize=True)
-            factors_model = ifu_factors(factors, fiber_groups=fiber_groups, normalize=True)
+            gradient_model = model.ifu_gradient(coeffs, x=x, y=y, normalize=True)
+            factors_model = model.ifu_factors(factors, fiber_groups=fiber_groups, normalize=True)
             plot_gradient_fit(self._slitmap, z/mu, gradient_model=gradient_model, factors_model=factors_model, telescope="Sci", axs=axs)
 
         return x, y, z, coeffs, factors
@@ -3312,10 +3305,9 @@ class RSS(FiberRows):
         else:
             raise ValueError(f"Keyword argument `groupby` has to be given if `factors` is given: {groupby = }")
 
-        pars = list(coeffs) + list(factors)
         fiber_groups = self._get_fiber_groups(by=groupby)
         x, y = self._slitmap["xpmm"].data, self._slitmap["ypmm"].data
-        joint_model, gradient_model, factors_model = ifu_joint_model(pars=pars, x=x, y=y, fiber_groups=fiber_groups, normalize=normalize, return_components=True)
+        joint_model, gradient_model, factors_model = IFUGradient.ifu_joint_model(coeffs, factors, x=x, y=y, fiber_groups=fiber_groups, normalize=normalize, return_components=True)
 
         return joint_model, gradient_model, factors_model
 

python/lvmdrp/functions/run_twilights.py
@@ -21,7 +21,7 @@ from lvmdrp.core.tracemask import TraceMask
 from lvmdrp.core.spectrum1d import Spectrum1D
 from lvmdrp.core.rss import RSS, lvmFrame
 from lvmdrp.core.fluxcal import butter_lowpass_filter
-from lvmdrp.core.fit_profile import ifu_factors, ifu_gradient
+from lvmdrp.core.fit_profile import IFUGradient
 from lvmdrp.core import dataproducts as dp
 from lvmdrp.core.plot import plt, slit, plot_gradient_fit, save_fig
 from lvmdrp import main as drp
@@ -182,17 +182,11 @@ def to_native_wave(rss, wave=None):
     else:
         raise ValueError(f"missing wavelength trace information: {rss._wave_trace = }")
 
-    new_rss = RSS(
+    new_rss = copy(rss)
+    new_rss.setData(
         data=np.zeros((rss._fibers, wave.shape[1]), dtype="float32"),
         error=np.zeros((rss._fibers, wave.shape[1]), dtype="float32"),
-        mask=np.zeros((rss._fibers, wave.shape[1]), dtype="bool"),
-        wave=wave,
-        cent_trace=rss._cent_trace,
-        width_trace=rss._width_trace,
-        wave_trace=rss._wave_trace,
-        lsf_trace=rss._lsf_trace,
-        slitmap=rss._slitmap,
-        header=rss._header
+        mask=np.zeros((rss._fibers, wave.shape[1]), dtype="bool")
     )
 
     # reset header keywords to match original wavelength grid state
@@ -333,7 +327,7 @@ def combine_twilight_sequence(in_fiberflats: List[str], out_fiberflat: str,
 
     mflat = copy(fflats[0])
     mflat._data = biweight_location([fflat._data for fflat in fflats], axis=0, ignore_nan=True)
-    mflat._error = np.sqrt(biweight_location([fflat._error**2 for fflat in fflats], axis=0, ignore_nan=True)) / len(fflats)
+    mflat._error = np.sqrt(biweight_location([fflat._error**2 for fflat in fflats], axis=0, ignore_nan=True))
 
     # mask invalid pixels
     mflat._mask |= np.isnan(mflat._data) | (mflat._data <= 0) | np.isinf(mflat._data)
@@ -346,8 +340,6 @@ def combine_twilight_sequence(in_fiberflats: List[str], out_fiberflat: str,
     mflat.set_wave_trace(TraceMask.from_spectrographs(*[TraceMask.from_file(in_wave) for in_wave in in_waves]))
     mflat.set_lsf_trace(TraceMask.from_spectrographs(*[TraceMask.from_file(in_lsf) for in_lsf in in_lsfs]))
     mflat = to_native_wave(mflat)
-    mflat._error = None
-    mflat._mask = None
     mflat.writeFitsData(out_fiberflat, replace_masked=False)
 
     return mflat
@@ -629,7 +621,7 @@ def fit_skyline_flatfield(in_sciences, in_mflat, out_mflat, cwave, dwave=8, norm
 
         log.info(f"fitting gradient and factors around sky line @ {cwave:.2f} Angstrom for '{imagetyp}' exposure {expnum = }")
         x, y, z, coeffs, factor = fscience.fit_ifu_gradient(cwave=cwave, dwave=dwave, coadd_method="fit")
-        gradient_model = ifu_gradient(coeffs, x=x, y=y, normalize=True)
+        gradient_model = IFUGradient.ifu_gradient(coeffs, x=x, y=y, normalize=True)
         factors.append(factor)
         log.info(f" factors         = {np.round(factor, 4)}")
         log.info(f" gradient across = {bn.nanmax(gradient_model)/bn.nanmin(gradient_model):.4f}")
@@ -639,8 +631,8 @@ def fit_skyline_flatfield(in_sciences, in_mflat, out_mflat, cwave, dwave=8, norm
 
         axs = [fig.add_subplot(gs_gra[i, j]) for j in range(5)]
         axs[0].set_ylabel(f"{expnum = }", fontsize="large")
-        gradient_model = ifu_gradient(coeffs, x, y, normalize=True)
-        factors_model = ifu_factors(factor, fiber_groups, normalize=True)
+        gradient_model = IFUGradient.ifu_gradient(coeffs, x, y, normalize=True)
+        factors_model = IFUGradient.ifu_factors(factor, fiber_groups, normalize=True)
         plot_gradient_fit(fscience._slitmap, z, gradient_model, factors_model, telescope="Sci", marker_size=15, axs=axs, labels=i==0)
 
     factor_mean = np.mean(factors, axis=0)
@@ -660,8 +652,8 @@ def fit_skyline_flatfield(in_sciences, in_mflat, out_mflat, cwave, dwave=8, norm
     axs = [fig.add_subplot(gs_gra[-2, j]) for j in range(5)]
     axs[0].set_ylabel("combined exposure", fontsize="large")
     x, y, skyline_slit, coeffs, factor = science.fit_ifu_gradient(cwave=cwave, dwave=dwave, groupby="spec", coadd_method="fit")
-    gradient_res = ifu_gradient(coeffs, x=x, y=y, normalize=True)
-    factors_final = ifu_factors(factor, fiber_groups, normalize=True)
+    gradient_res = IFUGradient.ifu_gradient(coeffs, x=x, y=y, normalize=True)
+    factors_final = IFUGradient.ifu_factors(factor, fiber_groups, normalize=True)
     plot_gradient_fit(science._slitmap, skyline_slit, gradient_res, factors_final, telescope="Sci", marker_size=15, axs=axs, labels=False)
     log.info(f"all fibers factors       = {np.round(factor, 4)}")
     log.info(f"residual gradient across = {bn.nanmax(gradient_res)/bn.nanmin(gradient_res):.4f}")
@@ -678,7 +670,7 @@ def fit_skyline_flatfield(in_sciences, in_mflat, out_mflat, cwave, dwave=8, norm
             factor[i] = bn.nanmedian(skyline_slit[slitmap["spectrographid"]==i+1])
         log.info(f"sky fiber factors    = {np.round(factor, 4)}")
 
-    flatfield_corr = ifu_factors(factor, fiber_groups)
+    flatfield_corr = IFUGradient.ifu_factors(factor, fiber_groups)
     flatfield_corr = np.repeat(factor, science._fibers / factor.size)
 
     science_corr = science / flatfield_corr[:, None]