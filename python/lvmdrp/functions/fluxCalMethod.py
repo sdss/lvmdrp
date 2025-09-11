8a2f5c64aff7db8bd84d93a1e8f4c091bacbb1d2
EvgeniiaEgorova <e.egorova@uni-heidelberg.de>
2025-09-11 11:35:09 +0200

    Updated warning on tellurics correction

--- python/lvmdrp/functions/fluxCalMethod.py
+++ python/lvmdrp/functions/fluxCalMethod.py
@@ -1509,7 +1509,7 @@ def correct_tellurics(wave, std_spec, lsf, in_rss, chan):
     :return:
     """
     std_telluric_corrected = std_spec.copy()
-    warnings.warn(f"Tellurics correction is not implemented yet. Skipping correction.")
+    log.warning(f"Tellurics correction is not implemented yet. Skipping correction.")
     return std_telluric_corrected
 
     telluric_file = os.path.join(os.getenv("LVMCORE_DIR"), "etc", "skytable.fits")
